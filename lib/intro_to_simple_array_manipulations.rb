886e0b565994e16da0187219028d29be58bb2f64
Bethany Jenkins <bethanykjenkins@rocketmail.com>
2019-09-09 02:29:16 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -19,47 +19,6 @@ end
 
 def pop_with_args
   @dog_breeds = ["Great Dane", "Golden Retriever", "Chihuahua", "Shiba Inu"]
-    @small_dogs = @dog_breeds.pop(2)
+  @small_dogs = @dog_breeds.pop(2)
 end
 
-def using_shift
-  @my_favorite_cities = ["Lagos", "Cape Town", "Nairobi", "San Francisco", "Gaborone", "New York", "Berlin", "London"]
-  @im_so_over_this_city = @my_favorite_cities.using_shift
-end
-
-def shift_with_args
-  @ice_cream_brands = ["Blue Bell Creameries", "Ben & Jerry's", "Baskin Robbins", "Braum's", "Breyer's"]
-  @brands_removed = @ice_cream_brands.shfit(2)
-end
-
-def using_concat
-  @my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
-  more_favs = ["sports cars", "flatiron school"]
-  @all_my_favs = @my_favorite_things.concat(more_favs)
-end
-
-def using_insert
-  @list_of_programming_languages = ["Ruby", "JavaScript", "Java", "C#", "Objective C", "C++", "PHP"]
-  @another_language = "Python"
-  @new_array = @list_of_programming_languages.insert(@another_language)
-end
-
-def using_uniq
-  haircuts = ["Pixie", "Bob", "Mohawk", "Crew Cut", "Linka", "Wheeler", "Bob"]
-  new_array = haircuts.uniq
-end
-
-def using_flatten
-  instruments = ["Saxophone", ["Piano", "Trumpet"], "Violin", "Drums", "Flute"]
-  flat_array = instruments.using_flatten
-end
-
-def using_delete
-  instructors = ["Josh", "Steven", "Sophie", "Steven", "Amanda", "Steven"]
-  no_offense_steven = instructors.delete ("Steven")
-end
-
-def using_delete_at
-  famous_robots = ["Johnny 5", "R2D2", "Robocop"]
-  deleted_robot = famous_robots.delete(2)
-end
