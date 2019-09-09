a45eb81895e07b08f6f99e23be174637a63f1f7f
Bethany Jenkins <bethanykjenkins@rocketmail.com>
2019-09-09 02:14:53 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,18 +1,18 @@
 
 
-def using_push
+def using_push (array, string)
   colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
   next_color = "violet"
   updated_array = colors_in_the_rainbow.push (next_color)
 end
 
-def using_unshift
+def using_unshift (array, string)
   bouroughs_in_nyc = ["Brooklyn", "Queens", "Manhattan", "Bronx"]
     @new_neighborhood = "Staten Island"
     @updated_array = using_unshift(bouroughs_in_nyc, @new_neighborhood)
 end
 
-def using_pop
+def using_pop (array)
   @continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
   @deleted_string = @continents.pop
 end
