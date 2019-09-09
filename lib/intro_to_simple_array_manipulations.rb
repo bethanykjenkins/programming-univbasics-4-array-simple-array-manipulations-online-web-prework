41bc664379e1ab21459d0f934e4759b76afb7399
Bethany Jenkins <bethanykjenkins@rocketmail.com>
2019-09-09 02:21:09 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -33,4 +33,9 @@ def shift_with_args
 end
 
 def using_concat
-  
+  @my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
+  more_favs = ["sports cars", "flatiron school"]
+  @all_my_favs = @my_favorite_things.concat(more_favs)
+end
+
+def
